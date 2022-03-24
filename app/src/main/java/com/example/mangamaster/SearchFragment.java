6e2c607a16dfcb97547cac24dae44b8891814e8c
bryan <bigavilanezb00@elpuig.xeill.net>
2022-03-24 12:17:08 +0100

    24 03 2022

--- app/src/main/java/com/example/mangamaster/SearchFragment.java
+++ app/src/main/java/com/example/mangamaster/SearchFragment.java
@@ -2,18 +2,62 @@ package com.example.mangamaster;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.mangamaster.databinding.FragmentSearchBinding;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class SearchFragment extends Fragment {
+    private FragmentSearchBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return (binding = FragmentSearchBinding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.viewPager.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                switch (position) {
+                    case 0: default:
+                        return new BuscadorFragment();
+                    case 1:
+                        return new TopMangasFragment();
+                }
+            }
+
+            @Override
+            public int getItemCount() {
+                return 2;
+            }
+        });
+
+        new TabLayoutMediator(binding.tabLayout, binding.viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position) {
+                    case 0: default:
+                        tab.setText("TODO");
+                        break;
+                    case 1:
+                        tab.setText("LO MÁS VISTO");
+                        break;
+                }
+            }
+        }).attach();
+
     }
 }
